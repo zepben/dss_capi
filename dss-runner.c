4fc1c6bcc93aa59f298dd1dfd7f98c1f67a286e8
Alex Lourie <alex.lourie@zepben.com>
2023-08-15 11:50:52 +0930

    Fix the test runner binary
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- dss-runner.c
+++ dss-runner.c
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[] ) {
 
+    char cwd[1000];
     char cmd[1000];
     char path[200];
     char *file_names[4] = { "main.dss", "Main.dss", "master.dss", "Master.dss" };
@@ -22,14 +23,35 @@ int main(int argc, char *argv[] ) {
         sprintf(path, "%s", argv[1]);
     }
 
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+       perror("getcwd() error");
+       exit(1);
+    }
+
+    // Check if the path exists
+    file_path = malloc(sizeof(char) * 2000);
+    sprintf(file_path, "%s/%s", cwd, path);
+    if ( access(file_path, F_OK) != 0 ) {
+        printf("The provided path '%s' doesn't exist!\n", file_path);
+        exit(1);
+    }
+
+    // Look out for the master dss file
     for (int i = 0; i < 4; i++) {
         file_path = malloc(sizeof(char) * 2000);
-        sprintf(file_path, "%s/%s", path, file_names[i]);
+        sprintf(file_path, "%s/%s/%s", cwd, path, file_names[i]);
 
         if (access(file_path, F_OK) == 0) {
           printf("File %s found\n", file_path);
           break;
+        } else {
+            file_path = NULL;
         }
+    }
+
+    if (file_path == NULL) {
+        printf("Can't find a file to work with!\n");
+        exit(1);
     }
 
     connect_rabbitmq("localhost", 5672, "hc", "password", "opendss", "amq.direct", 100);
